e1305cb5399677dc9593bc4f99ac04c046d00851
austinmcel <austinmcel@gmail.com>
2026-03-05 21:58:51 -0500

    Added user story #3

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home.index'),
     path('about', views.about, name='home.about'),
+    path('admin-dashboard', views.admin_dashboard, name = 'home.admin-dashboard' )
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from cart.models import Item
 
 # Create your views here.
 def index(request):
@@ -13,3 +14,23 @@ def about(request):
     return render(request,
                   'home/about.html',
                   {'template_data': template_data})
+
+def admin_dashboard(request):
+    if not request.user.is_superuser:
+        return redirect('home.index')
+    template_data = {}
+    template_data['title'] = 'Admin Dashboard'
+    items = Item.objects.all()
+    movie_counts = {}
+    for item in items:
+        name = item.movie.name
+        if name in movie_counts:
+            movie_counts[name] += item.quantity
+        else:
+            movie_counts[name] = item.quantity
+    most_purchased = None
+    if movie_counts:
+        top_movie = max(movie_counts, key = movie_counts.get)
+        most_purchased = { 'movie_name' : top_movie, 'total_purchased': movie_counts[top_movie]}
+    template_data['most_purchased'] = most_purchased
+    return render(request, 'home/admin_dashboard.html' , {'template_data': template_data})
